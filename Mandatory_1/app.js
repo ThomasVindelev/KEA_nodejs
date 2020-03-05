0be00cb0893df9baee29033f624916c3827030ba
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2020-03-05 17:29:43 +0100

    Work done on commands page

--- Mandatory_1/app.js
+++ Mandatory_1/app.js
@@ -1,23 +1,36 @@
 const express = require("express")
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000)
+const port = process.env.PORT ? process.env.PORT: 3000
+
+const server = app.listen(port, (error) => {
+    if (error) {
+        console.log("Server failed")
+    } else {
+        console.log(`Launched on port ${port}`)
+    }
+});
+
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
+    return res.sendFile(__dirname + "/public/index.html")
 })
 
 app.get("/commands", (req, res) => {
-    res.sendFile(__dirname + "/public/commands.html")
+    return res.sendFile(__dirname + "/public/commands.html")
 })
 
 app.get("/REST", (req, res) => {
-    res.sendFile(__dirname + "/public/REST.html")
+    return res.sendFile(__dirname + "/public/REST.html")
 })
 
 app.get("/syntax", (req, res) => {
-    res.sendFile(__dirname + "/public/syntax.html")
+    return res.sendFile(__dirname + "/public/syntax.html")
 })
 
+app.get("/jQuery", (req, res) => {
+    return res.sendFile(__dirname + "/public/jQuery.html")
+})
--- Mandatory_1/public/script.js
+++ Mandatory_1/public/script.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     let nav_bar = $("#nav-bar")
     nav_bar.append(
         `<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background-image: linear-gradient(#00010D, #222329);">
-        <a class="navbar-brand" href="#">node.js documentation</a>
+        <a class="navbar-brand" href="/">node.js documentation</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
