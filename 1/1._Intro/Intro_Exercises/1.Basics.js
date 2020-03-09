0275cd8ae5fc82d12299d4093861fcc3360b744a
ThomasVindelev <thom891b@stud.kea.dk>
2020-03-09 22:34:41 +0100

    Expanded syntax page

--- 1/1._Intro/Intro_Exercises/1.Basics.js
+++ 1/1._Intro/Intro_Exercises/1.Basics.js
@@ -1,6 +1,7 @@
 var firstName = "Thomas";
 var lastName = "Vindelev";
 let myName = `My name is ${firstName}`
+var cars = ["Cars", 123]
 
 console.log(myName)
 console.log("My first name is", firstName, "and my last name is", lastName);
@@ -15,3 +16,27 @@ var newYear2 = +(year) + number
 console.log(Number(year)+number)
 console.log(newYear)
 console.log(newYear2)
+
+for (i = 0; i < cars.length; i++) {
+    console.log(cars[i])
+}
+
+function calculate(first, second, operation) { 
+    return operation(first, second) 
+} 
+
+function add(first, second) { 
+    return first + second 
+} 
+
+function subtract(first, second) { 
+    return first - second 
+} 
+
+let operations = [add, subtract] 
+
+console.log(calculate(2, 3, operations[0])) 
+console.log(calculate(2, 3, operations[1])) 
+
+// 5 
+// -1
